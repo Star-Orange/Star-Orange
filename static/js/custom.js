cc411964492b487eab359f746f067edbe1f53252
clollipop <1792945133@qq.com>
2023-12-02 08:04:39 +0000

    更改文件路径 Updated By Github Actions

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,1 +1,1 @@
-var down=document.getElementById("scroll-down");down?.remove(),console.log(document.getElementsByClassName("menus_items")[1]),document.getElementsByClassName("menus_items")[1].style.display="none!important";
+var down=document.getElementById("scroll-down");down?.remove();
