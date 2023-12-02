8996e6b6ffa9e2b48ff435e4f43f5eb0d36375e9
clollipop <1792945133@qq.com>
2023-12-02 07:51:22 +0000

    增加网站标题恶搞，增加上次未添加的黑白背景图片切换，标题颜色切换，完成基本博客改造 Updated By Github Actions

---
+++ static/js/title.js
@@ -0,0 +1,1 @@
+var titleTime,OriginTitile=document.title;document.addEventListener("visibilitychange",(function(){document.hidden?(document.title="👀别跑家人~",clearTimeout(titleTime)):(document.title="🐖可算回来了～",titleTime=setTimeout((function(){document.title=OriginTitile}),2e3))}));
